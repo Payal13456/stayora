0dc98ad5eaa72d2dfce3ae30b0de86c3eb3600e9
payal13456 <payalnihore@gmail.com>
2026-07-19 12:32:37 +0530

    commit sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteUrl: "https://myhomivo.com",
+
+  generateRobotsTxt: true,
+
+  sitemapSize: 5000,
+
+  changefreq: "daily",
+
+  priority: 0.7,
+};
--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,17 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import "../styles/globals.css";
 import Script from "next/script";
 import ChatbotWidget from "../components/ChatbotWidget";
+import SEO from "../components/SEO";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <SEO
+        title="Homivo | Student PG & Hostel"
+        description="Find verified PGs and hostels."
+        image="https://myhomivo.com/logo.png"
+        url="https://myhomivo.com/"
+      />
       <GoogleTagManager gtmId="GTM-KVH3WTXH" />
       <Component {...pageProps} />
       <ChatbotWidget />
